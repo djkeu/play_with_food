a575bd3734a62ff847aa0ee715492971b5d45667
djkeu <mrkooij@gmail.com>
2024-05-13 08:52:34 +0200

    Create and call title_card()

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 
 
 def main():
-    ...
+    title_card()
+
+
+def title_card():
+    print("\n\t\tPlay with food")
+    print("URL: https://github.com/djkeu/play_with_food/tree/main\n")
 
 
 if __name__ == "__main__":
